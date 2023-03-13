346e5b1b8e33b2bc3724572b25ec0a95fecb3d66
Кирилл <kiryamiass7@gmail.com>
2023-03-13 12:29:18 +0500

    Добавлена дата создания короткой ссылки Осталось добавить обновление счетчика по ajax

--- main/shorturl/models.py
+++ main/shorturl/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 
-class Link(models.Model):
+class TimeStamp(models.Model):
+    create = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Link(TimeStamp, models.Model):
     fullurl = models.CharField(max_length=500)
     newurl = models.CharField(max_length=500)
     count = models.IntegerField(default=0)
 
     def __str__(self):
-        return f'Полная сслыка: {self.fullurl} Коротка ссылка: {self.newurl} Обращений: {self.count}'
+        return f'Полная сслыка: {self.fullurl} Коротка ссылка: {self.newurl} Дата создания: {TimeStamp.create}Обращений: {self.count}'
 
 # Create your models here.
